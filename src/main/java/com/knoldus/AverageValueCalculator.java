436bee9784aacfad584808ffe62ae8ce59568f19
niraj-knoldus-1331 <niraj.kumar@knoldus.com>
2023-02-06 12:27:41 +0530

    Spike Detection

---
+++ src/main/java/com/knoldus/AverageValueCalculator.java
@@ -0,0 +1,25 @@
+package com.knoldus;
+
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+public class AverageValueCalculator extends ProcessWindowFunction<SensorMeasurement, AverageValue, Integer, TimeWindow> {
+    @Override
+    public void process(Integer integer, ProcessWindowFunction<SensorMeasurement, AverageValue, Integer, TimeWindow>.Context context, Iterable<SensorMeasurement> elements, Collector<AverageValue> out) throws Exception {
+        int sensorId = -1;
+        float currentValue = 0;
+        float sum = (float) 0;
+        int count = 0;
+
+        for (SensorMeasurement element : elements) {
+            sensorId = element.getSensorId();
+            sum = sum + element.getTemperature();
+            count = count + 1;
+            currentValue = element.getTemperature();
+        }
+
+        AverageValue result = new AverageValue(sensorId, currentValue, sum / count);
+        out.collect(result);
+    }
+}
